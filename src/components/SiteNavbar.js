b73b72e6bd5d4eb8ce75c13470713b729ef27abc
Jonny Vickers <jonathanbvickers@gmail.com>
2022-01-27 17:53:59 +0000

    quick search nav bar

--- src/components/SiteNavbar.js
+++ src/components/SiteNavbar.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const SiteNavbar = () => {
   const navigate = useNavigate()
   
   const [ searchValue, setSearchValue ] = useState('')
+  const [ quickSearchFilm, setQuickSearchFilm ] = useState({})
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -16,17 +18,51 @@ const SiteNavbar = () => {
   const updateSearchValue = (e) => {
     setSearchValue(e.target.value)
   }
+
+  const goBack = () => {
+    navigate(-1)
+  }
+
+  useEffect(() => {
+    
+    const getPreSearchData = async () => {
+      try {
+        const { data } = await axios.get(`http://www.omdbapi.com/?t=${searchValue}&apikey=66b63fd8`)
+        console.log(data)
+        setQuickSearchFilm(data)
+        // console.log(data.Response)
+      } catch (error) {
+        console.log(error.message)
+      }
+      
+    }
+    getPreSearchData()
+  }, [searchValue])
   return (
     <ul>
       <div className="nav-container">
         <li>
           <Link to='/'>Film Search</Link>
         </li>
-        <li>
+        <li id='search-li'>
           <form onSubmit={handleSubmit}>
-            <input onChange={updateSearchValue} type="text" name='search' placeholder='Search' />
+            <div id="search-box">
+              <input onChange={updateSearchValue} type="text" name='search' placeholder='Search' value={searchValue}/>
+              {quickSearchFilm.Response === 'True' && 
+                <div className='quick-search-container'>
+                  <img src={quickSearchFilm.Poster} alt={quickSearchFilm.Title}/>
+                  <div className='quick-search-text'>
+                    <h3>{quickSearchFilm.Title}</h3>
+                    <p>{quickSearchFilm.Released}</p>
+                  </div>
+                  
+                </div>}
+            </div>
             <input type="submit" value='Search' />
           </form>
+        </li>
+        <li>
+          <button onClick={goBack}>Go back</button>
         </li>
       </div>
       
